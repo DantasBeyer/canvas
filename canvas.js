0785ccb26db7535679f0722adb74d7f8b4ec0aec
Joao Beyer <100128679+DantasBeyer@users.noreply.github.com>
2022-04-11 23:39:07 +0200

    First Commit
    Beginn of the Canvas project

---
+++ canvas.js
@@ -0,0 +1,25 @@
+//the link to my canvas in index.html
+const canvas = document.querySelector ('canvas');
+const ctx = canvas.getContext ('2d');
+
+//the size of my canvas
+canvas.width = 800;
+canvas.height = 800;
+
+// ctx.fillRect( x axis , y axis , width , height ); 
+
+//some objects in one array 
+let defenders = [
+carvajal = ctx.fillRect(50,100,50,50),
+pepe = ctx.fillRect(250,100,50,50),
+ramos = ctx.fillRect(450,100,50,50),
+marcelo = ctx.fillRect(650,100,50,50),
+casemiro = ctx.fillRect(350,300,50,50)
+ ];
+
+
+
+
+
+
+console.log(canvas);
